86f553b776907dcc623c75d2370eb66a7aae53a7
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2026-05-13 14:05:40 -0300

    Create 1187.c

---
+++ 1187.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    double m[12][12], soma = 0.0;
+    char operacao;
+    scanf("%c", &operacao);
+    setbuf(stdin, NULL);
+    for (int i = 0; i < 12; i++)
+        for (int j = 0; j < 12; j++) {
+            scanf("%lf", &m[i][j]);
+            if ( (i < j) && (i + j < 11) )  soma += m[i][j];
+        }
+    if (operacao == 'M') soma /= 30.0;
+    printf("%.1f\n", soma);
+    return 0;
+}
