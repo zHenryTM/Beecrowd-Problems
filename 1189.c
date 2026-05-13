7301fb3d05af40151bad4cc949182f9451fbb428
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2026-05-13 14:32:35 -0300

    Create 1189.c

---
+++ 1189.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+    float m[12][12], soma = 0;
+    char operacao;
+    
+    scanf("%c", &operacao);
+    
+    for (int i = 0; i < 12; i++)
+        for (int j = 0; j < 12; j++) {
+            scanf("%f", &m[i][j]);
+            if ( (i > j) && (i + j < 11) ) soma += m[i][j];
+        }
+        
+    if (operacao == 'M') soma /= 30.0;
+    
+    printf("%.1f\n", soma);
+ 
+    return 0;
+}
