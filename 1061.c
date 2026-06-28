0397ad80397c15b5fc853dc5a5f9648c8bcf621c
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2026-06-28 15:19:10 -0300

    Update 1061.c

--- 1061.c
+++ 1061.c
@@ -1,50 +1,24 @@
 #include <stdio.h>
 
-int converter_para_segundos(char hora[]) {
-    int h, m, s;
-    sscanf(hora, "%d : %d : %d", &h, &m, &s);
-    s += m * 60 + h * 3600;
-    return s;
-}
-
 int main() {
-    int dia_inicio, dia_fim, seg_inicio, seg_fim;
-    char dia[10], hora[20];
-    
-    scanf(" %[^\n]", dia);
-    sscanf(dia, "Dia %d", &dia_inicio);
-    
-    scanf(" %[^\n]", hora);
-    seg_inicio = converter_para_segundos(hora);
-    
-    scanf(" %[^\n]", dia);
-    sscanf(dia, "Dia %d", &dia_fim);
-    
-    scanf(" %[^\n]", hora);
-    seg_fim = converter_para_segundos(hora);
- 
-    // Calculando tempo do evento
-    int temp;
-    temp = (dia_fim - dia_inicio) * 24 * 3600;
-    temp += (seg_fim - seg_inicio);
-    
-    int d, h, m, s;
-    
-    d = temp / (3600 * 24);
-    temp %= (3600 * 24);
-    
-    h = temp / 3600;
-    temp %= 3600;
-    
-    m = temp / 60;
-    temp %= 60;
-    
-    s = temp;
-    
-    printf("%d dia(s)\n", d);
-    printf("%d hora(s)\n", h);
-    printf("%d minuto(s)\n", m);
-    printf("%d segundo(s)\n", s);
-    
+    int diaInicio, diaFim, hora, minuto, segundo, segundosInicio, segundosFim, duracao;
+
+    scanf("Dia %d", &diaInicio);
+    scanf("%d : %d : %d", &hora, &minuto, &segundo);
+    segundosInicio = hora * 3600 + minuto * 60 + segundo;
+
+    scanf(" Dia %d", &diaFim);
+    scanf("%d : %d : %d", &hora, &minuto, &segundo);
+    segundosFim = hora * 3600 + minuto * 60 + segundo;
+
+    duracao = (diaFim - diaInicio) * 86400 + segundosFim - segundosInicio;
+
+    printf("%d dia(s)\n", duracao / 86400);
+    duracao %= 86400;
+    printf("%d hora(s)\n", duracao / 3600);
+    duracao %= 3600;
+    printf("%d minuto(s)\n", duracao / 60);
+    printf("%d segundo(s)\n", duracao % 60);
+
     return 0;
 }
