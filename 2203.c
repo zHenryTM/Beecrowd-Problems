95fe84fdd9013911d15f61392fb302f96c1f0f51
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2026-09-16 19:47:06 -0300

    Update collision detection and enemy movement functions
    Refactor functions to use double for precision and add enemy movement logic.

--- 2203.c
+++ 2203.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
+#include <math.h>
 
-int pode_ser_atingido(int Xf, int Yf, int Xi, int Yi, int R) {
-    // Equa��o da Circunfer�ncia: (x - Xf)� + (y - Yf)� = R�
-    // (Xi - Xf)� + (Yi - Yf)� = R�
-    // (Xi� - 2 * Xi * Xf + Xf�) + (Yi� - 2 * Yi * Yf + Yf�) = R�
-    
-    return (Xi * Xi - 2 * Xi * Xf + Xf * Xf) + (Yi * Yi - 2 * Yi * Yf + Yf * Yf) == R * R;
+int pode_ser_atingido(double Xf, double Yf, double Xi, double Yi, double R) {    
+    return (Xi - Xf) * (Xi - Xf) + (Yi - Yf) * (Yi - Yf) <= R * R;
+}
+
+void atualizar_coord_inimigo(double *Xi, double *Yi, double Xf, double Yf, double Vi) {
+    double Wx = *Xi - Xf;
+    double Wy = *Yi - Yf;
+
+    double modulo_w = sqrt(Wx * Wx + Wy * Wy);
+
+    if (modulo_w > 0) {
+        *Xi = *Xi + (1.5 * Wx * Vi / modulo_w);
+        *Yi = *Yi + (1.5 * Wy * Vi / modulo_w);
+    } else {
+        // Já que o inimigo está no mesmo ponto que o jogador, posso escolher arbitrariamente a direção do movimento do inimigo,
+        // então, para facilitar os cálculos, escolhi a diretação horizontal para alterar, apenas, o eixo x.
+        
+        *Xi = *Xi + 1.5 * Vi;
+    }
 }
 
 int main() {
-    // Xfiddle, Yfiddle
-    // Xinvasor, Yinvasor
-    // velocidade_invasor
-    // raio_ult, raio_corvos
-
-    int Xf, Yf, Xi, Yi, Vi, Ru, Rc;
-    int R;
-
-    scanf("%d %d %d %d %d %d %d", &Xf, &Yf, &Xi, &Yi, &Vi, &Ru, &Rc);
-
-    R = Ru + Rc;
-
-    if (pode_ser_atingido(Xf, Yf, Xi, Yi, R))
-        printf("Y\n");
-    else
-        printf("N\n");
-
+    // Embora o enunciado diga que as entradas sejam inteiras, as leia como double, pois precisão nesse exercício é muito importante
+    double Xf, Yf, Xi, Yi, Vi, Ru, Rc;
+    double R;
+    
+    while (scanf("%lf %lf %lf %lf %lf %lf %lf", &Xf, &Yf, &Xi, &Yi, &Vi, &Ru, &Rc) != EOF) {
+        R = Ru + Rc;
+    
+        if (Vi > 0)
+            atualizar_coord_inimigo(&Xi, &Yi, Xf, Yf, Vi);
+    
+        if ( pode_ser_atingido(Xf, Yf, Xi, Yi, R) )
+            printf("Y\n");
+        else
+            printf("N\n");
+    }
 }
